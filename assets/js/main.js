31d87b182aa4111ed88974071c2e7b6888cf7e51
Asya Vee <nyayaoi@gmail.com>
2018-06-14 00:28:40 +0300

    colors and paddongs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@
 //Detecting toush devices and display grid card titles
 
 if (Modernizr.touch) {
-  $("h2[hidden='true']").removeClass("desktop-hidden");
+  $('.desktop-hidden').removeClass('desktop-hidden');
 };
